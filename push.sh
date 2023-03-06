d9a6389f9b51634dec014a1137ba1ddf445aced2
0strich <gee8195@naver.com>
2023-03-07 04:22:59 +0900

    update push

--- push.sh
+++ push.sh
@@ -1,16 +1,63 @@
+#!/bin/bash
+set -e
+
+# Default values for options
+PUSH_NODE=false
+
 type=$1
 message=$2
 
-push () {
+push() {
 	npm run $type
 	git add .
 	git commit -m "$message"
 	git push
 }
 
-case $1 in
-	patch) push;;
-	minor) push;;
-	major) push;;
-esac
+# Usage information
+function usage() {
+	echo "Usage: $0 [-n]"
+	echo "Options:"
+	echo "  -n  Push Node.js"
+}
 
+# Parse options
+while getopts ":n" opt; do
+	case ${opt} in
+	n)
+		PUSH_NODE=true
+		;;
+	\?)
+		usage
+		exit 1
+		;;
+	:)
+		echo "Invalid option: -$OPTARG requires an argument" 1>&2
+		usage
+		exit 1
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+
+# Push Node.js
+if $PUSH_NODE; then
+	case $1 in
+	patch | minor | major)
+		push
+		;;
+	*)
+		echo "Invalid option: $1" 1>&2
+		usage
+		exit 1
+		;;
+	esac
+fi
+
+# main
+git add .
+git commit -m "$1"
+git pusk
+
+# Final message
+echo "Complete."
