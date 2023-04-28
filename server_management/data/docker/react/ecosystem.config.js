935b7e47f173a05a3c81550b8356803c94df4ed7
0strich <gee8195@naver.com>
2023-04-28 19:00:16 +0900

    update ecosystme

--- server_management/data/docker/react/ecosystem.config.js
+++ server_management/data/docker/react/ecosystem.config.js
@@ -4,7 +4,8 @@ module.exports = {
       // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
       name: "react",
       // pm2로 실행될 파일 경로
-      script: "./node_modules/react-scripts/scripts/start.js",
+      script: "npm",
+      args: "start",
       watch: true,
       instances: 1, // 0으로 설정시 CPU 코어 수 만큼 프로세스 생성
       log_date_format: "YYYY-MM-DD HH:mm Z",
