6fedc72328b45ebc6dbbfa5d9f87e4538a1c9ef5
0strich <gee8195@naver.com>
2023-05-07 00:15:28 +0900

    update ecosysem

--- server_management/data/docker/react/ecosystem.config.js
+++ server_management/data/docker/react/ecosystem.config.js
@@ -6,16 +6,6 @@ module.exports = {
       args: "start",
       watch: true,
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      // 개발환경시 적용될 설정 지정
-      env: {
-        NODE_ENV: "production",
-        PORT: "80",
-      },
-      // 배포환경시 적용될 설정 지정
-      env_production: {
-        NODE_ENV: "production",
-        PORT: "80",
-      },
     },
   ],
 };
